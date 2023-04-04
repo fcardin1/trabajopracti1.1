a347fc8eca9ac5df53d6f698e086cfa8cf0669eb
fcardin1 <129565141+fcardin1@users.noreply.github.com>
2023-04-03 18:18:09 -0600

    Add files via upload

--- trabajopractico1/ar/org/centro8/curso/java/entities/Vehiculo.java
+++ trabajopractico1/ar/org/centro8/curso/java/entities/Vehiculo.java
@@ -4,6 +4,56 @@ public class Vehiculo {
     public String color;
     public String marca;
     public String modelo;
+    public String Radio;
+    
+    public Vehiculo(String color, String marca, String modelo, String radio) {
+        this.color = color;
+        this.marca = marca;
+        this.modelo = modelo;
+        Radio = radio;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo [color=" + color + ", marca=" + marca + ", modelo=" + modelo + ", Radio=" + Radio + "]";
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getRadio() {
+        return Radio;
+    }
+
+    public void setRadio(String radio) {
+        Radio = radio;
+    }
+
+    
+
+    
+    
     
     
 }
