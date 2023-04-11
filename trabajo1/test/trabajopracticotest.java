fd8c6f8f9055301ceca9c41329f591da99cf4887
fcardin1 <129565141+fcardin1@users.noreply.github.com>
2023-04-10 21:59:42 -0300

    Add files via upload

---
+++ trabajo1/test/trabajopracticotest.java
@@ -0,0 +1,17 @@
+package test;
+
+
+import entities.Autoclasico;
+import entities.Autonuevo;
+import entities.Vehiculo;
+
+public class trabajopracticotest {
+
+    public static void main(String[] args) {
+        System.out.println("auto clasico");
+       Vehiculo vehiculo1=new Vehiculo("gris", "renault", "clio", "aspen");
+       Autoclasico autoclasico1=new Autoclasico();
+       Autonuevo autonuevo1=new Autonuevo();
+    }
+    
+}
---
+++ trabajo1/trabajo1/ar/org/centro8/curso/java/entities/Radio.java
@@ -0,0 +1,7 @@
+package entities;
+
+public class Radio {
+    String marca;
+    int potencia;
+    
+}
